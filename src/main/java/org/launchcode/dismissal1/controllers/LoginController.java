c09b9c8214681ba3a928b50a5086ea8a0e3ae729
bgwtz4 <b.winston87@yahoo.com>
2019-10-27 08:24:26 -0500

    Buttons and inputs for login in page added.

--- src/main/java/org/launchcode/dismissal1/controllers/LoginController.java
+++ src/main/java/org/launchcode/dismissal1/controllers/LoginController.java
@@ -1,20 +1,30 @@
 package org.launchcode.dismissal1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
+@RequestMapping("login")
 public class LoginController {
+
     @RequestMapping(value = "")
-    @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String login(Model model) {
+        model.addAttribute("title", "The Dismissal App");
+        return "login/login";
     }
 
-    @RequestMapping(value="goodbye")
+    @RequestMapping(value= "log")
     @ResponseBody
-public String Goodbye() {
+    public String Goodbye() {
         return "goodbye";
+    }
+
+    @RequestMapping(value= "iforgot")
+    @ResponseBody
+    public String Hey(){
+        return "hey";
     }
 }
