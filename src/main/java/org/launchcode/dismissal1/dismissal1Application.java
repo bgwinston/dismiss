fbcd47c721b1a5aeaf5cbb23ba38f61bb6a045ec
bgwtz4 <b.winston87@yahoo.com>
2019-11-17 23:33:30 -0600

    Fragments added to "early" templates

--- src/main/java/org/launchcode/dismissal1/dismissal1Application.java
+++ src/main/java/org/launchcode/dismissal1/dismissal1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class dismissal1Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(dismissal1Application.class, args);
 	}
