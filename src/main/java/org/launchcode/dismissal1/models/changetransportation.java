0a3a46488b1b1b813676804e087a98914954b430
bgwtz4 <b.winston87@yahoo.com>
2019-11-30 09:05:11 -0600

    Created changetransportation table and Dao to track transportation changes

---
+++ src/main/java/org/launchcode/dismissal1/models/changetransportation.java
@@ -0,0 +1,78 @@
+package org.launchcode.dismissal1.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+public class changetransportation {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    //@NotNull
+    @Size(min = 3, max= 8, message = "Select Current Transportation.")
+    private String transportation;
+
+    //@NotNull
+    @Size(min = 3, max= 8, message = "Select Transportation change.")
+    private String transportationchange;
+
+    //@NotNull
+    @Size(min = 3, max= 8, message = "Enter date in mm:dd:yyyy format.")
+    private String datec;
+
+    //@NotNull
+    @Size(min = 3, max=25, message= "Write a note for your records.")
+    private String notes1;
+
+//@Size(min = 3)
+//format of date and time of submission
+//private LocalDateTime submission=LocalDateTime.now();
+//DateTimeFormatter forsubmission = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+//private String formattedDate = submission.format(forsubmission);
+
+    public changetransportation(String transportation, String transportationchange, String datec, String notes1) {
+        this.transportation = transportation;
+        this.transportationchange=transportationchange;
+        this.datec = datec;
+        this.notes1 = notes1;
+        //this.formattedDate = formattedDate;
+        }
+    public int getId() {
+        return id;
+        }
+
+    public String getTransportation() {
+        return transportation;
+        }
+
+    public void setTransportation(String transportation ) {
+        this.transportation = transportation;
+        }
+
+    public String getTransportationchange() {
+        return transportationchange;
+        }
+
+    public void setTransportationchange(String transportationchange) {
+        this.transportationchange = transportationchange;
+        }
+
+    public String getDatec() {
+        return datec;
+        }
+
+    public void setDatec(String datec) {
+        this.datec = datec;
+        }
+
+    public String getNotes1() {
+        return notes1;
+        }
+
+    public void setNotes1(String notes1) {
+        this.notes1 = notes1;
+        }
+}
---
+++ src/main/java/org/launchcode/dismissal1/models/data/ChangetransportationDao.java
@@ -0,0 +1,13 @@
+package org.launchcode.dismissal1.models.data;
+
+import org.launchcode.dismissal1.models.changetransportation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface ChangetransportationDao extends CrudRepository<changetransportation,Integer> {
+
+}
