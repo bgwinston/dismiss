cf5eff4868553a54875c657e853983519428909a
Britaney <b.winston87@yahoo.com>
2020-01-14 20:52:27 -0600

    spring security dependency added

---
+++ src/main/java/org/launchcode/dismissal1/websecurityconfig.java
@@ -0,0 +1,24 @@
+package org.launchcode.dismissal1;
+
+/*import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class websecurityconfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                //antMatchers is where you put pages that you want to allow//
+                .antMatchers( "/login", "/newaccount").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+   }
+}
+*/
